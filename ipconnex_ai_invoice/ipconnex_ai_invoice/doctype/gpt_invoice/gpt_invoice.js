574f97da3b2efec5d999b2edf387d17d11b26436
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2023-12-11 13:28:02 +0100

    commit

--- ipconnex_ai_invoice/ipconnex_ai_invoice/doctype/gpt_invoice/gpt_invoice.js
+++ ipconnex_ai_invoice/ipconnex_ai_invoice/doctype/gpt_invoice/gpt_invoice.js
@@ -1,8 +1,9 @@
 frappe.ui.form.on('GPT Invoice', {
     refresh: function(frm) {
 
+        $("input[data-fieldname='generated_sales']").prop("disabled",true);
+        $("input[data-fieldname='generated_purchase']").prop("disabled",true);
 
-        
     }
                     
 });
