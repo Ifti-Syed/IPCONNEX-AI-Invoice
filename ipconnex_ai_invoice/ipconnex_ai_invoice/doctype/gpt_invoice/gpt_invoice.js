6a2c5bad7c662ec72e86178bfdaa20da6e66dc6d
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2023-12-11 19:47:00 +0100

    commit

--- ipconnex_ai_invoice/ipconnex_ai_invoice/doctype/gpt_invoice/gpt_invoice.js
+++ ipconnex_ai_invoice/ipconnex_ai_invoice/doctype/gpt_invoice/gpt_invoice.js
@@ -3,30 +3,27 @@ frappe.ui.form.on('GPT Invoice', {
 
         $("input[data-fieldname='generated_sales']").prop("disabled",true);
         $("input[data-fieldname='generated_purchase']").prop("disabled",true);
-
     }
-                    
 });
 frappe.ui.form.on('GPT Invoice Item', {
-      
     item_code: function(frm, cdt, cdn) { 
             let item=locals[cdt][cdn];
             frappe.db.get_doc("Item",item.item_code).then((res)=>{
               item.item_rate=res.standard_rate;
               item.item_qty=1;
               item.item_amount=res.standard_rate;
-              frm.refresh_field("items_list");
+              frm.refresh_field("invoice_items");
             });
     },
     item_qty: function(frm, cdt, cdn) { 
             let item=locals[cdt][cdn];
             item.item_amount=item.item_rate*item.item_qty;
-            frm.refresh_field("items_list");
+            frm.refresh_field("invoice_items");
     },
     item_rate: function(frm, cdt, cdn) { 
         let item=locals[cdt][cdn];
         item.item_amount=item.item_rate*item.item_qty;
-        frm.refresh_field("items_list");
+        frm.refresh_field("invoice_items");
     },
     
 });
