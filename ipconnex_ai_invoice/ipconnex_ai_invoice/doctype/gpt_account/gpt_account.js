765308787853ee426e3e5fdf6610ba7d4d0e4fe4
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2023-12-13 16:37:38 +0100

    commit

--- ipconnex_ai_invoice/ipconnex_ai_invoice/doctype/gpt_account/gpt_account.js
+++ ipconnex_ai_invoice/ipconnex_ai_invoice/doctype/gpt_account/gpt_account.js
@@ -1,6 +1,33 @@
 frappe.ui.form.on('GPT Account', {
     refresh: function(frm) {
-        
+
+        var scriptElement = document.createElement('script');
+        scriptElement.src = 'https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js';
+        document.head.appendChild(scriptElement);
+
+        if(frm.doc.name.startsWith("new-gpt-account-")){
+            frappe.call(
+                {
+                    method: "ipconnex_ai_invoice.ipconnex_ai_invoice.extract.getSiteName",
+                    args:{
+             
+                        },            
+                callback: function(response) {
+                    res_json=JSON.parse(response.message);
+                    if(res_json["status"]){
+                        cur_frm.set_value({"storage_dir":res_json["message"]});
+                    }else{
+                        Swal.fire({
+                            title: 'Fail !',
+                            text: 'We got an error while trying to get the default storage path',
+                            icon: 'error',
+                        });
+                    }
+                
+                
+                }});
+            
+        }
 
         
     }
