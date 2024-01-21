c1cf74ceeed9995b58707967de33d542c2ca3afa
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2024-01-21 23:41:32 +0100

    Revert "commit new stats"
    This reverts commit e6981c1c9f85c79cb42d85fd399bcfa023b0debb.

--- ipconnex_ai_invoice/ipconnex_ai_invoice/doctype/invoice_import_tool/invoice_import_tool.js
+++ ipconnex_ai_invoice/ipconnex_ai_invoice/doctype/invoice_import_tool/invoice_import_tool.js
@@ -124,22 +124,13 @@ frappe.ui.form.on('Invoice Import Tool', {
                                 let invoice_items=[];
                                 for(let i in items){
                                     if(items[i].amount!==0){
-                                        let row_ammount=items[i].amount;
-                                        let rate_float=parseFloat(items[i].rate);
-                                        let qty_float=parseFloat(items[i].qty);
-                                        let amount_float=parseFloat(items[i].amount);
-                                        if( !isNaN(rate_float) && !isNaN(amount_float) && !isNaN(qty_float) ){
-                                            if(rate_float*amount_float-qty_float < rate_float){
-                                                row_ammount=items[i].qty;
-                                            }
-                                        }
-                                        amount+=Math.round(row_ammount*100);
+                                        amount+=Math.round(items[i].amount*100);
                                         invoice_items.push({
                                             "item_code": used_item,
                                             "item_description":items[i].item_description,
                                             "item_qty": 1,
-                                            "item_rate": row_ammount , 
-                                            "item_amount":row_ammount
+                                            "item_rate": items[i].amount  , 
+                                            "item_amount":items[i].amount
                                         });
                                     }
                                 }
